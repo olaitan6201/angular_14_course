d6de75f263c9b3517eae81dcd602fb3b3ba24049
Olaitan O. Habeeb <olaitan6201@gmail.com>
2022-08-31 06:09:39 +0000

    created carts component

---
+++ src/app/carts/carts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../books/book.directive';
+import { CartService } from './cart.service';
+
+@Component({
+  selector: 'app-carts',
+  templateUrl: './carts.component.html',
+  styleUrls: ['./carts.component.scss']
+})
+export class CartsComponent implements OnInit {
+
+  carts : Book[] = []
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.carts = this.cartService.get()
+  }
+
+}
