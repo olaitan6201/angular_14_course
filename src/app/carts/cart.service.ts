40a90c1d6d26fb8a820f6666bcdca46854e511f2
Olaitan O. Habeeb <olaitan6201@gmail.com>
2022-08-31 06:10:00 +0000

    added carts service

---
+++ src/app/carts/cart.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Book } from '../books/book.directive';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  carts : Book[] = []
+
+  constructor() { }
+
+  add(book: Book): void{
+    this.carts.push(book)
+  }
+
+  get() : Book[]{
+    return this.carts.slice()
+  }
+}
