5aeb19ee1d498437e636d2d3259d7f027f802521
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-31 08:54:37 +0100

    added auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 import { CartsComponent } from './carts/carts.component';
 import { BooksComponent } from './books/books.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +7,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: BooksComponent},
-  { path: 'carts', component: CartsComponent}
+  { path: 'carts', component: CartsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
