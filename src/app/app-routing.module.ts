8c7fff630863e516441f0bc7f25e1003d41e7689
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-31 07:49:10 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { CartsComponent } from './carts/carts.component';
+import { BooksComponent } from './books/books.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BooksComponent},
+  { path: 'carts', component: CartsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
