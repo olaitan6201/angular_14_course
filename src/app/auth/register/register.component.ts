b05c626c95b724cba6695691857ecbc34ea775e4
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-09-01 09:52:45 +0100

    added form validation in register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  registerForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password: new FormControl(null, Validators.required),
+    password_confirmation: new FormControl(null, Validators.required)
+  })
+  
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, Validators.required),
+      password_confirmation: new FormControl(null, Validators.required)
+    })
+  }
+
+  onSubmit(){
+    console.log(this.registerForm);
+    
+    const { email, password, password_confirmation } = this.registerForm.value 
+    if(password !== password_confirmation) return alert('Password confirmation field incorrect');
+
+    //do something
+  }
+
+  fieldStatusText(field: any): string{
+    if(field.errors){
+      const { errors } = field
+      if(errors.required) return "This field is required"
+      return "The input value is invalid"
+    }
+    return ""
   }
 
 }
