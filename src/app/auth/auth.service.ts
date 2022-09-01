09ce1ba481fc77a171698f937ec4d00ec2c5000a
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-09-01 22:42:21 +0100

    created auth service file

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { signIn, createUser } from './../firebase/auth.firebase';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isAuthenticated:boolean = false
+
+  isLoading:boolean = false
+
+  constructor() { }
+
+  signIn(email: string, password: string) : void {
+    if(this.isLoading || this.isAuthenticated) return
+    this.isLoading = true
+    signIn(email, password).then((res: any) => res && console.log(res))
+    .finally(()  => this.isLoading = false )
+  }
+
+  register(email: string, password: string) : void{
+    if(this.isLoading || this.isAuthenticated) return
+
+    this.isLoading = true
+    
+    createUser(email, password).then((res: any) => {
+      if(res) console.log(res);
+    })
+    .finally(() => this.isLoading = false)
+  }
+
+}
