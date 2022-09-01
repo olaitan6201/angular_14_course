d0636d4703ef2664dcc5b1ffd0415f3fffb8f33f
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-09-01 22:22:28 +0100

    created firebase config file and added firebase configurations

---
+++ src/app/firebase/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0LQaOKymC13IxH8LKGQM_tEpAu62-x3U",
+  authDomain: "angular14bookshop.firebaseapp.com",
+  projectId: "angular14bookshop",
+  storageBucket: "angular14bookshop.appspot.com",
+  messagingSenderId: "329815781565",
+  appId: "1:329815781565:web:65573c098 1c72fad8587d4"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
